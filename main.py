a687712dd1769d6577f74854b6d20a775a3eb620
Mohamed Gupta <philjallman+fti@gmail.com>
2016-07-31 23:14:58 +0000

    Betterized it

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from os import path
 from slacker import Slacker
 
 
-def boost_tweets(api):
+def boost_tweets(tw, slack):
     targetfile = '/home/pi/git/boostguy/targets.p'
     sincefile = '/home/pi/git/boostguy/since_ids.p'
     blockfile = '/home/pi/git/boostguy/blocks.p'
-
     if path.isfile(targetfile):
         targets = pickle.load(open(targetfile, 'r'))
     else:
@@ -22,74 +21,80 @@ def boost_tweets(api):
     else:
         blocks = []
 
-    slack = Slacker(slack_config.key)
-
     if not targets:
         print 'No targets'
         return
 
+    for blocked_target in [b for b in blocks if b in targets]:
+        targets.remove(blocked_target)
+
     for target in targets:
         try:
-            api.CreateFriendship(screen_name=target)
+            tw.CreateFriendship(screen_name=target)
         except twitter.error.TwitterError, e:
-            if e[0][0]['code'] == 162:
+            if e[0][0]['code'] == 88:
+                slack.chat.post_message('#testbed', 'Rate limited')
+                return
+            elif e[0][0]['code'] in (136, 162):
                 blocks.append(target)
                 slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
                 slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
                 targets.remove(target)
-            if e[0][0]['code'] == 88:
-                print 'Rate limited!'
-        
-        if target not in blocks:
-            if target in since_ids:
-                since_id = since_ids[target]
-            else:
-                since_id = 0
+                targets.next()
+                continue
 
-            try:
-                tweets = api.GetUserTimeline(screen_name=target, since_id=since_id, include_rts=False)
-                if len(tweets) == 0:
-                    print 'No new tweets for ' + target
-                else:
-                    new_since_id = tweets[0].AsDict()['id']
-                    for tweet in tweets:
-                        try:
-                            api.PostRetweet(status_id=tweet.AsDict()['id'])
-                            since_ids[target] = new_since_id
-                        except twitter.error.TwitterError, e:
-                            if e[0][0]['code'] == 162:
-                                blocks.append(target)
-                                slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
-                                slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
-                                targets.remove(target)
-                            if e[0][0]['code'] == 88:
-                                print 'Rate limited!'
-
-            except twitter.error.TwitterError, e:
-                print e[0][0]
-                if e[0][0]['code'] == 162:
-                    blocks.append(target)
-                    slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
-                    slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
-                    targets.remove(target)
+        if target in since_ids:
+            since_id = since_ids[target]
         else:
-            print 'Checking {0} for some reason...'.format(target)
-            slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
-            slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
+            since_id = 0
+
+        try:
+            tweets = tw.GetUserTimeline(screen_name=target, since_id=since_id, include_rts=False)
+        except twitter.error.TwitterError, e:
+            if e[0][0]['code'] == 88:
+                slack.chat.post_message('#testbed', 'Rate limited')
+                return
+            elif e[0][0]['code'] in (136, 162):
+                blocks.append(target)
+                slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
+                slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
+                targets.remove(target)
+                continue
+            else:
+                slack.chat.post_message('#testbed', 'User timeline for {0} {1}'.format(target, e))
+            continue
+
+        if since_id == 0:
+            tweets = [tweets[0]]
+
+        for tweet in reversed(tweets):
+            try:
+                tw.PostRetweet(status_id=tweet.AsDict()['id'])
+                since_ids[target] = tweet.AsDict()['id']
+            except twitter.error.TwitterError, e:
+                if e[0][0]['code'] == 88:
+                    slack.chat.post_message('#testbed', 'Rate limited')
+                    return
+                elif e[0][0]['code'] in (136, 162):
+                    blocks.append(target)
+                    targets.remove(target)
+                    slack.chat.post_message('#testbed', 'Blocked on RT by ' + target + '\n' + e)
+                else:
+                    slack.chat.post_message('#testbed', e)
 
     targets = [t for t in set(targets)]
     pickle.dump(targets, open(targetfile, 'wb'))
     pickle.dump(since_ids, open(sincefile, 'wb'))
     pickle.dump([b for b in set(blocks)], open(blockfile, 'wb'))
 
-
 if __name__ == '__main__':
     cred = next(acct for acct in twitter_config.accounts if acct['username'] == 'BoostedThat4ya')
     try:
-        api = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
+        tw = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
                           access_token_key=cred['access_token_key'], access_token_secret=cred['access_token_secret'])
-    except:
-        print 'Failed to authenticate.'
+        slack = Slacker(slack_config.key)
+    except Exception, e:
+        print e
         exit()
 
-    boost_tweets(api)
+    boost_tweets(tw, slack)
