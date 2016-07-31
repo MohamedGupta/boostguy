ea636c4c83d1ae7060ac4b653e50a70aa3fe0e83
Mohamed Gupta <philjallman+fti@gmail.com>
2016-07-31 18:29:21 +0000

    Fixed block/error handling

--- main.py
+++ main.py
@@ -1,55 +1,80 @@
 #!/usr/bin/env python
-import twitter, twitter_config, slack_config
-import requests, json
+import twitter, twitter_config, slack_config, requests, json, pickle
 from os import path
 from slacker import Slacker
-import pickle
 
-targetfile = '/home/pi/git/boostguy/targets.p'
-sincefile = '/home/pi/git/boostguy/since_ids.p'
-
-cred = next(acct for acct in twitter_config.accounts if acct['username'] == 'BoostedThat4ya')
-if path.isfile(targetfile):
-    targets = pickle.load(open(targetfile, 'r'))
-if path.isfile(sincefile):
-    since_ids = pickle.load(open(sincefile, 'r'))
-else:
-    since_ids = dict()
-slack = Slacker(slack_config.key)
 
 def boost_tweets(api):
+    targetfile = '/home/pi/git/boostguy/targets.p'
+    sincefile = '/home/pi/git/boostguy/since_ids.p'
+    blockfile = '/home/pi/git/boostguy/blocks.p'
+
+    if path.isfile(targetfile):
+        targets = pickle.load(open(targetfile, 'r'))
+    else:
+        targets = []
+    if path.isfile(sincefile):
+        since_ids = pickle.load(open(sincefile, 'r'))
+    else:
+        since_ids = dict()
+    if path.isfile(blockfile):
+        blocks = pickle.load(open(blockfile, 'r'))
+    else:
+        blocks = []
+
+    slack = Slacker(slack_config.key)
+
     if not targets:
         print 'No targets'
         return
-    
+
     for target in targets:
-        if target in since_ids:
-            since_id = since_ids[target]
-        else:
-            since_id = 0
         try:
-            tweets = api.GetUserTimeline(screen_name=target, since_id=since_id, include_rts=False)
-        except twitter.error.TwitterError:
-            slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
-            targets.remove(target)
-            print 'Remaining targets: {0}'.format(', '.join(targets))
-            pickle.dump(targets, open(targetfile, 'wb'))
-            return
-
-        if len(tweets)==0:
-            print 'No new tweets for ' + target
-        else:
-            new_since_id = tweets[0].AsDict()['id']
+            api.CreateFriendship(screen_name=target)
+        except twitter.error.TwitterError, e:
+            print e
+            if e[0][0]['code'] == 162:
+                blocks.append(target)
+                slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
+                slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
+                targets.remove(target)
+        
+        if target not in blocks:
+            if target in since_ids:
+                since_id = since_ids[target]
+            else:
+                since_id = 0
             try:
-                api.PostRetweet(status_id=new_since_id)
-            except:
-                print 'Already retweeted {0}'.format(new_since_id)
-            since_ids[target] = new_since_id
+                tweets = api.GetUserTimeline(screen_name=target, since_id=since_id, include_rts=False)
+                if len(tweets) == 0:
+                    print 'No new tweets for ' + target
+                else:
+                    new_since_id = tweets[0].AsDict()['id']
+                    for tweet in tweets:
+                        try:
+                            api.PostRetweet(status_id=tweet.AsDict()['id'])
+                        except twitter.error.TwitterError, e:
+                            print 'Already retweeted {0}'.format(new_since_id)
+                            print e[0][0]
 
+                    since_ids[target] = new_since_id
+
+
+            except twitter.error.TwitterError, e:
+                if e[0][0]['code'] == 162:
+                    blocks.append(target)
+                    slack.chat.post_message('#signalboost', 'Signal boost guy has been blocked by ' + target)
+                    slack.chat.post_message('#signalboost', 'Cowards: ' + ':troll: '.join(blocks) + ' :troll:')
+                    targets.remove(target)
+
+    targets = [t for t in set(targets)]
+    pickle.dump(targets, open(targetfile, 'wb'))
     pickle.dump(since_ids, open(sincefile, 'wb'))
+    pickle.dump([b for b in set(blocks)], open(blockfile, 'wb'))
 
 
 if __name__ == '__main__':
+    cred = next(acct for acct in twitter_config.accounts if acct['username'] == 'BoostedThat4ya')
     try:
         api = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
                           access_token_key=cred['access_token_key'], access_token_secret=cred['access_token_secret'])
@@ -58,4 +83,3 @@ if __name__ == '__main__':
         exit()
 
     boost_tweets(api)
-
